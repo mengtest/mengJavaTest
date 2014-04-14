2473d99506c2706fae2d7b5f661dff204f403348
mengtest <244170255@qq.com>
2014-04-14 22:10:47 +0800

    第一次提交代码内容！！！

---
+++ src/main/java/meng/org/test/Hero.java
@@ -0,0 +1,16 @@
+package meng.org.test;
+
+public class Hero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("测试git项目"+System.currentTimeMillis());
+		System.out.println("时间"+"1397484522464"+" ,2014-04-14 22:09");
+
+	}
+
+}
