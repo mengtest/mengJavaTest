2d482837834e1077221c7bbd901a99224c78d550
mengtest <244170255@qq.com>
2014-04-15 20:51:25 +0800

    第三次提交内容，电脑间切换后是否正常！

---
+++ src/main/java/meng/org/test/HeroReq.java
@@ -0,0 +1,16 @@
+package meng.org.test;
+
+public class HeroReq {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HeroReq.java测试git项目"+System.currentTimeMillis());
+		System.out.println("时间"+"1397484522464"+" ,2014-04-15 20:42");
+
+	}
+
+}
